33e68bd6ae73d57e97dd8711c97a605ff51a565d
alihaiderdev <ah1675179@gmail.com>
2022-09-23 18:22:03 +0500

    working on stripe payment gateway integration

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,62 @@
-'use strict';
+"use strict";
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  //   // Method 1: Creating an entirely custom action
+  //   async exampleAction(ctx) {
+  //     try {
+  //       ctx.body = "ok";
+  //     } catch (err) {
+  //       ctx.body = err;
+  //     }
+  //   },
+  //   // Method 2: Wrapping a core action (leaves core logic in place)
+  //   async find(ctx) {
+  //     // some custom logic here
+  //     ctx.query = { ...ctx.query, local: "en" };
+  //     // Calling the default core action
+  //     const { data, meta } = await super.find(ctx);
+  //     // some more custom logic
+  //     meta.date = Date.now();
+  //     return { data, meta };
+  //   },
+  //   // Method 3: Replacing a core action
+  //   async findOne(ctx) {
+  //     const { id } = ctx.params;
+  //     const { query } = ctx;
+  //     const entity = await strapi
+  //       .service("api::restaurant.restaurant")
+  //       .findOne(id, query);
+  //     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+  //     return this.transformResponse(sanitizedEntity);
+  //   },
+
+  /**
+   * Create an order and sets up the stripe checkout session for the frontend
+   * @param {any} ctx
+   * @returns
+   */
+  async create(ctx) {
+    // const response = await super.create(ctx);
+    console.log(ctx.request);
+    const { product } = ctx.request.body;
+    if (!product) {
+      return ctx.throw(400, "Please specify products");
+    }
+
+    // const entry = await strapi.entityService.findOne(
+    //   "api::article.article",
+    //   product.id,
+    //   {
+    //     fields: ["title", "description"],
+    //     populate: { category: true },
+    //   }
+    // );
+  },
+}));
